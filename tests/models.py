f2102d6a08ac942914ec682a53361ac6d9dfefd3
Karl Hobley <karlhobley10@gmail.com>
2014-03-27 13:30:57 +0000

    Replace __unicode__ with  __str__ method in test models

--- tests/models.py
+++ tests/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 from modelcluster.tags import ClusterTaggableManager
 from taggit.models import TaggedItemBase
@@ -9,21 +10,24 @@ from modelcluster.fields import ParentalKey
 from modelcluster.models import ClusterableModel
 
 
+@python_2_unicode_compatible
 class Band(ClusterableModel):
     name = models.CharField(max_length=255)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 
+@python_2_unicode_compatible
 class BandMember(models.Model):
     band = ParentalKey('Band', related_name='members')
     name = models.CharField(max_length=255)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 
+@python_2_unicode_compatible
 class Album(models.Model):
     band = ParentalKey('Band', related_name='albums')
     name = models.CharField(max_length=255)
@@ -32,7 +36,7 @@ class Album(models.Model):
 
     sort_order_field = 'sort_order'
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
     class Meta:
@@ -42,40 +46,46 @@ class Album(models.Model):
 class TaggedPlace(TaggedItemBase):
     content_object = ParentalKey('Place', related_name='tagged_items')
 
+@python_2_unicode_compatible
 class Place(ClusterableModel):
     name = models.CharField(max_length=255)
     tags = ClusterTaggableManager(through=TaggedPlace)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
+
 
 class Restaurant(Place):
     serves_hot_dogs = models.BooleanField()
     proprietor = models.ForeignKey('Chef', null=True, blank=True, on_delete=models.SET_NULL, related_name='restaurants')
 
+@python_2_unicode_compatible
 class Dish(models.Model):
     name = models.CharField(max_length=255)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
+@python_2_unicode_compatible
 class Wine(models.Model):
     name = models.CharField(max_length=255)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
+@python_2_unicode_compatible
 class Chef(models.Model):
     name = models.CharField(max_length=255)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
+@python_2_unicode_compatible
 class MenuItem(models.Model):
     restaurant = ParentalKey('Restaurant', related_name='menu_items')
     dish = models.ForeignKey('Dish', related_name='+')
     price = models.DecimalField(max_digits=6, decimal_places=2)
     recommended_wine = models.ForeignKey('Wine', null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
 
-    def __unicode__(self):
+    def __str__(self):
         return "%s - %f" % (self.dish, self.price)

6e316f2c34795fd85e95c0042a0d9406939966b9
Matt Westcott <matt@west.co.tt>
2014-09-04 17:31:39 +0100

    add explicit MIDDLEWARE_CLASSES to runtests.py to silence warning in django 1.7

--- runtests.py
+++ runtests.py
@@ -21,6 +21,14 @@ if not settings.configured:
 
             'tests',
         ],
+        MIDDLEWARE_CLASSES = (
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.middleware.common.CommonMiddleware',
+            'django.middleware.csrf.CsrfViewMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.messages.middleware.MessageMiddleware',
+            'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        ),
         USE_TZ=True,
         TIME_ZONE='America/Chicago',
     )

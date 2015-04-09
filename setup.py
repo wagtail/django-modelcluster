f5b5df40e2a91434e093f373142f54d918554a1a
Matt Westcott <matt@west.co.tt>
2015-04-09 15:58:26 +0100

    prepare for v0.6 release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='django-modelcluster',
-    version='0.5',
+    version='0.6',
     description="Django extension to allow working with 'clusters' of models as a single unit, independently of the database",
     author='Matthew Westcott',
     author_email='matthew.westcott@torchbox.com',

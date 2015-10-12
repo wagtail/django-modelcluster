ba417180007cff3179bb281b48cc3c591cbf91af
Karl Hobley <karlhobley10@gmail.com>
2015-10-12 15:54:54 +0100

    Removed some Django 1.6 specific code
    This code was crashing due to form._raw_value being removed in Django 1.9. According to the comment, this code can now be removed as we no longer support Django 1.6

--- modelcluster/forms.py
+++ modelcluster/forms.py
@@ -94,45 +94,6 @@ class BaseChildFormSet(BaseTransientModelFormSet):
 
         return saved_instances
 
-    # Prior to Django 1.7, objects are deleted from the database even when commit=False:
-    # https://code.djangoproject.com/ticket/10284
-    # This was fixed in https://github.com/django/django/commit/65e03a424e82e157b4513cdebb500891f5c78363
-    # We rely on the fixed behaviour here, so until 1.7 ships we need to override save_existing_objects
-    # with a patched version.
-    def save_existing_objects(self, commit=True):
-        self.changed_objects = []
-        self.deleted_objects = []
-        if not self.initial_forms:
-            return []
-
-        saved_instances = []
-        try:
-            forms_to_delete = self.deleted_forms
-        except AttributeError:
-            forms_to_delete = []
-        for form in self.initial_forms:
-            pk_name = self._pk_field.name
-            raw_pk_value = form._raw_value(pk_name)
-
-            # clean() for different types of PK fields can sometimes return
-            # the model instance, and sometimes the PK. Handle either.
-            pk_value = form.fields[pk_name].clean(raw_pk_value)
-            pk_value = getattr(pk_value, 'pk', pk_value)
-
-            obj = self._existing_object(pk_value)
-            if form in forms_to_delete:
-                self.deleted_objects.append(obj)
-                # === BEGIN PATCH ===
-                if commit:
-                    obj.delete()
-                # === END PATCH ===
-                continue
-            if form.has_changed():
-                self.changed_objects.append((obj, form.changed_data))
-                saved_instances.append(self.save_existing(form, obj, commit=commit))
-                if not commit:
-                    self.saved_forms.append(form)
-        return saved_instances
 
 def childformset_factory(parent_model, model, form=ModelForm,
     formset=BaseChildFormSet, fk_name=None, fields=None, exclude=None,

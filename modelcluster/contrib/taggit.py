b02719716ca2a645cf268a53d7b571a993ef4b02
Matt Westcott <matt@west.co.tt>
2016-08-02 17:55:30 +0100

    Piggyback on django-taggit's self._to_tag_model_instances method to eliminate copy-and-paste

--- modelcluster/contrib/taggit.py
+++ modelcluster/contrib/taggit.py
@@ -1,8 +1,6 @@
 from __future__ import unicode_literals
 from __future__ import absolute_import
 
-from django.conf import settings
-from django.utils import six
 from taggit import VERSION as TAGGIT_VERSION
 from taggit.managers import TaggableManager, _TaggableManager
 from taggit.utils import require_instance_manager
@@ -43,53 +41,7 @@ class _ClusterTaggableManager(_TaggableManager):
 
     @require_instance_manager
     def add(self, *tags):
-        # Add `tags` (which may be either strings or tag objects) to this instance's tag list.
-
-        # This implementation is a copy of taggit's own logic from _TaggableManager.add
-        # except for the final part that creates the 'through' objects (where we need to
-        # push them to the tagged_item manager, rather than creating them as DB objects).
-        # This is currently in sync with django-taggit master as of 2016-04-26:
-        # https://github.com/alex/django-taggit/tree/1daae6fc83009ef86cc62445537746a08aab91aa
-
-        # First turn the 'tags' list (which may be a mixture of tag objects and
-        # strings which may or may not correspond to existing tag objects)
-        # into 'tag_objs', a set of tag objects
-        str_tags = set()
-        tag_objs = set()
-        for t in tags:
-            if isinstance(t, self.through.tag_model()):
-                tag_objs.add(t)
-            elif isinstance(t, six.string_types):
-                str_tags.add(t)
-            else:
-                raise ValueError("Cannot add {0} ({1}). Expected {2} or str.".format(
-                    t, type(t), type(self.through.tag_model())))
-
-        if getattr(settings, 'TAGGIT_CASE_INSENSITIVE', False):
-            # Some databases can do case-insensitive comparison with IN, which
-            # would be faster, but we can't rely on it or easily detect it.
-            existing = []
-            tags_to_create = []
-
-            for name in str_tags:
-                try:
-                    tag = self.through.tag_model().objects.get(name__iexact=name)
-                    existing.append(tag)
-                except self.through.tag_model().DoesNotExist:
-                    tags_to_create.append(name)
-        else:
-            # If str_tags has 0 elements Django actually optimizes that to not do a
-            # query.  Malcolm is very smart.
-            existing = self.through.tag_model().objects.filter(
-                name__in=str_tags
-            )
-
-            tags_to_create = str_tags - set(t.name for t in existing)
-
-        tag_objs.update(existing)
-
-        for new_tag in tags_to_create:
-            tag_objs.add(self.through.tag_model().objects.create(name=new_tag))
+        tag_objs = self._to_tag_model_instances(tags)
 
         # Now write these to the relation
         tagged_item_manager = self.get_tagged_item_manager()

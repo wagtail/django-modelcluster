12884bb8a4dedc3ea04d43cc3401b9b0dcb89ba4
Matt Westcott <matt@west.co.tt>
2019-04-02 21:43:03 +0100

    Remove Python 3.4 from tests and classifiers

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     packages=find_packages(exclude=('tests*',)),
     license='BSD',
     long_description=open('README.rst').read(),
+    python_requires=">=3.5",
     install_requires=[
         "pytz>=2015.2",
     ],
@@ -29,10 +30,10 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
         'Framework :: Django',
     ],
 )

eb5d7f213c6bb291f528c88bfbf645f891294459
Matt Westcott <matt@west.co.tt>
2020-01-06 18:36:20 +0000

    Version bump to 4.4.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='django-modelcluster',
-    version='4.4',
+    version='4.4.1',
     description="Django extension to allow working with 'clusters' of models as a single unit, independently of the database",
     author='Matthew Westcott',
     author_email='matthew.westcott@torchbox.com',

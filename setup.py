4279edd029a8227736ceb09214437bd7b0b71791
Matt Westcott <matt@west.co.tt>
2023-10-04 15:18:37 +0100

    Add Python 3.11 and 3.12 to testing matrix

--- setup.py
+++ setup.py
@@ -35,6 +35,8 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3 :: Only',
         'Framework :: Django',
     ],
